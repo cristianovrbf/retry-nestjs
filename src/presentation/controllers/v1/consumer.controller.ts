ab79de385fac0f2eb1423796251dbd26088c0f12
cristiano <cristianorbfilho@gmail.com>
2024-11-21 03:07:08 -0300

    refactor[consumer-server-nestjs]: delete unsed files and refac setup params

--- src/presentation/controllers/v1/consumer.controller.ts
+++ src/presentation/controllers/v1/consumer.controller.ts
@@ -1,10 +1,8 @@
 import { ConsumeExternalServiceUseCase } from "@/application/usecases/consume-external-service";
-import { RetryInterceptor } from "@/infrastructure/interceptors/retry.interceptor";
 import { Controller, Get, UseInterceptors } from "@nestjs/common";
 
 
 @Controller()
-@UseInterceptors(RetryInterceptor)
 export class ConsumerV1Controller{
 
     constructor(
